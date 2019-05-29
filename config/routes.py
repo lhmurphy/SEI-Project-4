2566c1bb491ec8f8808c778cb873d32f3a49dc07
Laura Murphy <laurahelenamurphy@gmail.com>
2019-05-29 10:43:37 +0100

    Login and register working in insomnia

--- config/routes.py
+++ config/routes.py
@@ -1,7 +1,7 @@
 from app import app
-from controllers import books
-from controllers import locations, reviews
+from controllers import books, locations, reviews, auth
 
 app.register_blueprint(books.router)
 app.register_blueprint(locations.router)
 app.register_blueprint(reviews.router)
+app.register_blueprint(auth.router)
