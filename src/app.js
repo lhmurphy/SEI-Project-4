5ca5d17eff4395c925872c4abd0d85ece5d35963
Laura Murphy <laurahelenamurphy@gmail.com>
2019-05-29 11:52:43 +0100

    NavBar created

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import { BrowserRouter } from 'react-router-dom'
+
+import 'bulma'
+
+import Navbar from './components/common/Navbar'
+
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello World!</h1>
+      <BrowserRouter>
+        <main>
+          <Navbar />
+        </main>
+      </BrowserRouter>
     )
   }
 }
---
+++ src/components/common/Navbar.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import { Link, withRouter } from 'react-router-dom'
+
+class Navbar extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = { active: false }
+
+    this.logout = this.logout.bind(this)
+    this.toggleActive = this.toggleActive.bind(this)
+  }
+
+  logout() {
+    this.props.history.push('/')
+  }
+
+  toggleActive() {
+    this.setState({ active: !this.state.active })
+  }
+
+  componentDidUpdate(prevProps) {
+    if(prevProps.location.pathname !== this.props.location.pathname) {
+      this.setState({ active: false })
+    }
+
+  }
+
+  render() {
+    return (
+      <nav className="navbar is-dark">
+        <div className="container">
+
+          <div className="navbar-brand">
+            {/* Branding and burger menu */}
+            <Link to="/" className="navbar-item display is-size-4">Wanderlist</Link>
+
+            <a
+              role="button"
+              className={`navbar-burger${this.state.active ? ' is-active' : ''}`}
+              onClick={this.toggleActive}
+            >
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
+
+          <div className={`navbar-menu${this.state.active ? ' is-active' : ''}`}>
+
+            <div className="navbar-end">
+              <Link to="/register" className="navbar-item">Register</Link>
+              <Link to="/login" className="navbar-item">Login</Link>
+              <a className="navbar-item" onClick={this.logout}>Logout</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    )
+  }
+}
+
+// `withRouter` gives the Navbar `history` via props
+export default withRouter(Navbar)
