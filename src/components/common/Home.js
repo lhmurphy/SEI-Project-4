00e7eed5f4cdf9b21207c485896b4dde195ef2c1
Laura Murphy <laurahelenamurphy@gmail.com>
2019-06-04 06:38:57 +0100

    Small changes

--- src/components/common/Home.js
+++ src/components/common/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Auth from '../../lib/Auth'
 import { Link } from 'react-router-dom'
-
+import axios from 'axios'
 
 class Home extends React.Component{
   constructor(props) {
@@ -12,24 +12,32 @@ class Home extends React.Component{
     }
   }
 
+  componentDidMount() {
+    axios.get('/api/books')
+      .then(res => this.setState({ books: res.data }))
+  }
+
   render() {
+    console.log(this.state.books)
+    console.log(this.state.author)
+
     return(
       <div className="home">
         <div className="container full-height">
           <div className="columns">
-            <div className="column is-two-fifths">
-            </div>
             <div className="column">
-              <h1>Welcome to Wanderlist</h1>
-              <p>
-                Travelling somewhere? Match your next read to your destination with Wanderlist. Books set in locations around the world!...
-                <br />
-                {!Auth.isAuthenticated() && <Link to="/register" className="button is-danger">Register</Link>}
-                {!Auth.isAuthenticated() && <Link to="/login" className="button is-danger">Login</Link>}
-                <br />
-                {Auth.isAuthenticated() && <Link to="/books" className="button is-danger">Browse books...</Link>}
+              <div className="hero">
 
-              </p>
+                <h1>Welcome to Wanderlist</h1>
+                <div>
+                  Travelling somewhere? Match your next read to your destination with Wanderlist. Books set in locations around the world!...
+                  <br />
+                  {!Auth.isAuthenticated() && <Link to="/register" className="button is-danger">Register</Link>}
+                  {!Auth.isAuthenticated() && <Link to="/login" className="button is-danger">Login</Link>}
+                  <br />
+                  {Auth.isAuthenticated() && <Link to="/books" className="button is-danger"><h2>Discover Great Books</h2></Link>}
+                </div>
+              </div>
             </div>
           </div>
         </div>
